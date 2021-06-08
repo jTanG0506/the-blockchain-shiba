137828239c1d15f3f024879c728d802502888107
Jonathan Tang <hello@jonathantang.dev>
2021-06-08 23:09:24 +0100

    Create genesis, state and tx files

---
+++ database/state.go
@@ -0,0 +1,107 @@
+package database
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+type State struct {
+	Balances  map[Account]uint
+	txMempool []Tx
+
+	dbFile *os.File
+}
+
+func NewStateFromDisk() (*State, error) {
+	cwd, err := os.Getwd()
+	if err != nil {
+		return nil, err
+	}
+
+	genFilePath := filepath.Join(cwd, "database", "genesis.json")
+	gen, err := loadGenesis(genFilePath)
+	if err != nil {
+		return nil, err
+	}
+
+	balances := make(map[Account]uint)
+	for account, balance := range gen.Balances {
+		balances[account] = balance
+	}
+
+	txFilePath := filepath.Join(cwd, "database", "tx.db")
+	txs, err := os.OpenFile(txFilePath, os.O_APPEND|os.O_RDWR, 0600)
+	if err != nil {
+		return nil, err
+	}
+
+	scanner := bufio.NewScanner(txs)
+	state := &State{balances, make([]Tx, 0), txs}
+
+	for scanner.Scan() {
+		if err := scanner.Err(); err != nil {
+			return nil, err
+		}
+
+		var tx Tx
+		json.Unmarshal(scanner.Bytes(), &tx)
+
+		if err := state.apply(tx); err != nil {
+			return nil, err
+		}
+	}
+
+	return state, nil
+}
+
+func (s *State) Add(tx Tx) error {
+	if err := s.apply(tx); err != nil {
+		return err
+	}
+
+	s.txMempool = append(s.txMempool, tx)
+	return nil
+}
+
+func (s *State) Persist() error {
+	mempool := make([]Tx, len(s.txMempool))
+	copy(mempool, s.txMempool)
+
+	for i := 0; i < len(mempool); i++ {
+		txJson, err := json.Marshal(mempool[i])
+		if err != nil {
+			return err
+		}
+
+		if _, err = s.dbFile.Write(append(txJson, '\n')); err != nil {
+			return err
+		}
+
+		s.txMempool = s.txMempool[1:]
+	}
+
+	return nil
+}
+
+func (s *State) apply(tx Tx) error {
+	if tx.IsReward() {
+		s.Balances[tx.To] += tx.Value
+		return nil
+	}
+
+	if tx.Value > s.Balances[tx.From] {
+		return fmt.Errorf("Insufficient balance")
+	}
+
+	s.Balances[tx.From] -= tx.Value
+	s.Balances[tx.To] += tx.Value
+
+	return nil
+}
+
+func (s *State) Close() {
+	s.dbFile.Close()
+}
