19258250c720d1ae3aa5771ca50837fff3c08643
Jonathan Tang <hello@jonathantang.dev>
2021-07-19 22:09:53 +0100

    Add tests for signing TXs with keystore account

--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -4,11 +4,16 @@ import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
+	"io/ioutil"
 	"testing"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/jTanG0506/go-blockchain/database"
+	"github.com/jTanG0506/go-blockchain/fs"
 )
+
+const testKsPassword = "password"
 
 func TestSign(t *testing.T) {
 	privKey, err := ecdsa.GenerateKey(crypto.S256(), rand.Reader)
@@ -39,5 +44,81 @@ func TestSign(t *testing.T) {
 
 	if account.Hex() != recoveredAccount.Hex() {
 		t.Fatalf("msg was signed by %s but signature recover produced account %s", account.Hex(), recoveredAccount.Hex())
+	}
+}
+
+func TestSignTxWithKeystoreAccount(t *testing.T) {
+	tmpDir, err := ioutil.TempDir("", "wallet_test")
+	if err != nil {
+		t.Fatalf("unable to create temporary directory. %s", err.Error())
+	}
+	defer fs.RemoveDir(tmpDir)
+
+	toshi, err := NewKeystoreAccount(tmpDir, testKsPassword)
+	if err != nil {
+		t.Errorf("unable to create keystore account. %s", err.Error())
+	}
+
+	jtang, err := NewKeystoreAccount(tmpDir, testKsPassword)
+	if err != nil {
+		t.Errorf("unable to create keystore account. %s", err.Error())
+	}
+
+	tx := database.NewTx(toshi, jtang, 100, 1, "")
+	signedTx, err := SignTxWithKeystoreAccount(
+		tx,
+		toshi,
+		testKsPassword,
+		GetKeystoreDirPath(tmpDir),
+	)
+	if err != nil {
+		t.Fatalf("unable to sign transaction with private key. %s", err.Error())
+	}
+
+	ok, err := signedTx.IsSigAuthentic()
+	if err != nil {
+		t.Fatalf("unable to determine whether signature is authentic. %s", err.Error())
+	}
+
+	if !ok {
+		t.Fatalf("signature on transaction is not authentic. %s", err.Error())
+	}
+}
+
+func TestSignedForgedTxWithKeystoreAccount(t *testing.T) {
+	tmpDir, err := ioutil.TempDir("", "wallet_test")
+	if err != nil {
+		t.Fatalf("unable to create temporary directory. %s", err.Error())
+	}
+	defer fs.RemoveDir(tmpDir)
+
+	hacker, err := NewKeystoreAccount(tmpDir, testKsPassword)
+	if err != nil {
+		t.Errorf("unable to create keystore account. %s", err.Error())
+	}
+
+	toshi, err := NewKeystoreAccount(tmpDir, testKsPassword)
+	if err != nil {
+		t.Errorf("unable to create keystore account. %s", err.Error())
+	}
+
+	forgedTx := database.NewTx(toshi, hacker, 100, 1, "")
+	signedTx, err := SignTxWithKeystoreAccount(
+		forgedTx,
+		hacker,
+		testKsPassword,
+		GetKeystoreDirPath(tmpDir),
+	)
+	if err != nil {
+		t.Fatalf("unable to sign transaction with private key. %s", err.Error())
+	}
+
+	ok, err := signedTx.IsSigAuthentic()
+	if err != nil {
+		t.Fatalf("unable to determine whether signature is authentic. %s", err.Error())
+	}
+
+	if ok {
+		t.Fatalf("signature on transaction should not be authentic. %s", err.Error())
 	}
 }
